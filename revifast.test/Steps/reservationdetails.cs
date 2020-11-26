b3a59b565588371373ece4038ff8409825595879
Lutimi <misaticona@hotmail.com>
2020-11-26 02:00:22 -0500

    feature reservation details

---
+++ revifast.test/Steps/reservationdetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace revifast.test.Steps
+{
+    [Binding]
+    public sealed class reservationdetails
+    {
+        [Given(@"a verfied user")]
+        public void GivenAVerfiedUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the user want to validate his reservation information")]
+        public void GivenTheUserWantToValidateHisReservationInformation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the user click on the reservation information icon")]
+        public void WhenTheUserClickOnTheReservationInformationIcon()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the system will show the reservation information")]
+        public void ThenTheSystemWillShowTheReservationInformation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
