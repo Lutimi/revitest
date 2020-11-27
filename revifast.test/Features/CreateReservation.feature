e4f6f7804493930784e091fdc0fec4a1d006e490
Lutimi <misatiocona@hotmail.com>
2020-11-26 20:58:43 -0500

    Lutimi
    Version Final

---
+++ revifast.test/Features/CreateReservation.feature
@@ -0,0 +1,14 @@
+﻿Feature: CreateReservation
+	In order to get my service done
+	As a client
+	I want to be able to make a reservation
+
+Scenario: Client creates reservation
+	Given the client specifies valid data for a reservation
+	When the client sends this reservation data to the system
+	Then the system will successfuly save the reservation
+
+Scenario: Client fails to create reservation
+	Given the client specifies invalid data for a reservation
+	When the client sends this reservation data to the system
+	Then the system will not save the reservation
---
+++ revifast.test/Steps/SearchProviderSteps.cs
@@ -0,0 +1,74 @@
+﻿using Revifast.Entities;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace revifast.test.Steps
+{
+    class LEmpresas
+    {
+        public LEmpresas()
+        {
+            Empresas.Add(new Empresa { EmpresaId = 100, Nombre = "Tesla", Ruc = "RM34M", Telefono = "948018312", Correo = "teslamotor@gmail" });
+        }
+
+        List<Empresa> Empresas = new List<Empresa>();
+
+        public List<Empresa> Where(int id)
+        {
+            if (id == 1)
+            {
+                return Empresas;
+            }
+            else return null;
+        }
+
+    }
+
+    class Context
+    {
+        public LEmpresas Empresas = new LEmpresas();
+        public LReservations Reservas = new LReservations();
+
+    }
+
+    [Binding]
+    public class SearchProviderSteps
+    {
+        Context _context = new Context();
+        Empresa provider;
+        int providerId;
+        List<Empresa> empresas;
+
+        [Given(@"the client specifies valid data to look for a provider")]
+        public void GivenTheClientSpecifiesValidDataToLookForAProvider()
+        {
+            providerId = 1;
+        }
+        
+        [Given(@"the client specifies invalid data to look for a provider")]
+        public void GivenTheClientSpecifiesInvalidDataToLookForAProvider()
+        {
+            providerId = -12;
+        }
+        
+        [When(@"the client sends this data to the system")]
+        public void WhenTheClientSendsThisDataToTheSystem()
+        {
+            empresas = _context.Empresas.Where(id: providerId);
+        }
+        
+        [Then(@"the system will successfuly return a list of providers")]
+        public void ThenTheSystemWillSuccessfulyReturnAListOfProviders()
+        {
+            Assert.IsNotEmpty(empresas);
+        }
+        
+        [Then(@"the system will return an error")]
+        public void ThenTheSystemWillReturnAnError()
+        {
+            Assert.IsNull(empresas);
+        }
+    }
+}
