b4f95dccc4fffd852fce490ef9b2fe2c73fc6f15
zaj-e <carbonelmarcelo@gmail.com>
2020-11-26 20:48:07 -0500

    ESTOY PUSHEANDO A DEVELOP

---
+++ Revifast.test/Features/SearchService.feature
@@ -0,0 +1,15 @@
+﻿Feature: Search Provider
+	In order to make a reservation
+	As a car owner
+	I want to look for a technical provider
+
+Scenario: Successful Search
+	Given the client specifies valid data to look for a provider
+	When the client sends this data to the system
+	Then the system will successfuly return a list of providers
+
+
+Scenario: Unsuccessful Search
+	Given the client specifies invalid data to look for a provider
+	When the client sends this data to the system
+	Then the system will return an error
---
+++ Revifast.test/Steps/CreateReservationSteps.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+
+namespace revifast.test.Steps
+{
+    class LReservations
+    {
+        public LReservations()
+        {
+        }
+
+        List<Reserva> Reservas = new List<Reserva>();
+
+
+        public void Add (Reserva r)
+        {
+            Reservas.Add(r);
+        }
+
+        public List<Reserva>  GetAll()
+        {
+            return Reservas;
+        }
+
+    }
+
+    [Binding]
+    public class CreateReservationSteps
+    {
+        Context _context = new Context();
+        Reserva reservation;
+        int providerId;
+        List<Reserva> reservations;
+
+        [Given(@"the client specifies valid data for a reservation")]
+        public void GivenTheClientSpecifiesValidDataForAReservation()
+        {
+            reservation = new Reserva { ReservaId = 100, Fecha = "29/09/1999", Hora = "13:00GMT-5", Observaciones = "Mmmm" };
+        }
+        
+        [Given(@"the client specifies invalid data for a reservation")]
+        public void GivenTheClientSpecifiesInvalidDataForAReservation()
+        {
+            reservation = new Reserva { ReservaId = -1400, Fecha = "29/09/1999", Hora = "13:00GMT-5", Observaciones = "Mmmm" };
+        }
+
+        [When(@"the client sends this reservation data to the system")]
+        public void WhenTheClientSendsThisReservationDataToTheSystem()
+        {
+            _context.Reservas.Add(reservation);
+        }
+        
+        [Then(@"the system will successfuly save the reservation")]
+        public void ThenTheSystemWillSuccessfulySaveTheReservation()
+        {
+            Assert.IsNotEmpty(_context.Reservas.GetAll());
+        }
+        
+        [Then(@"the system will not save the reservation")]
+        public void ThenTheSystemWillNotSaveTheReservation()
+        {
+            Assert.IsNotEmpty(_context.Reservas.GetAll());
+        }
+    }
+}
